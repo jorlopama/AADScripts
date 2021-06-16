d1c54d06e4f58909673d9808ba9e18c7caa03325
Jorge Lopez <43187678+jorlopama@users.noreply.github.com>
2021-06-16 16:49:08 -0400

    Update Add-BulkAuthMethods_DelegatedMSALv2.ps1

--- Add-BulkAuthMethods_DelegatedMSALv2.ps1
+++ Add-BulkAuthMethods_DelegatedMSALv2.ps1
@@ -1,17 +1,34 @@
+ <#Notes 
+ SCRIPT TO PRE-POPULATE AZUREAD AUTHENTICATION METHODS (MFA) USING GRAPH API
+  Author: Jorge Lopez
+  1) This is a simple loop reading data from a csv file, you can be as creative as you like using other data sources
+  2) This script does NOT validate if a user already has an auth phone defined, if it does it will more likely fail
+  3) More logic should be added to validate if a user already has something populated, since a PUT call should be used instead of POST
+  4) CSV file needs to have at least 3 headers (userPrincipalName, objectid and authphone) 
+      *Userprincipalname = Obvious
+      *objectid = of course the objectid from the user
+      *authphone = this would be the phone number to populate, needs to have the format : +xx xxx xxxx
+      
+THIS CODE-SAMPLE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED 
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR 
+FITNESS FOR A PARTICULAR PURPOSE.
+THIS SAMPLE IS NOT SUPPORTED UNDER ANY MICROSOFT STANDARD SUPPORT PROGRAM OR SERVICE
+#>
+
 # Variables that include the resources, clientid and tenant id, importing msal.ps module 
 Import-module MSAL.PS
-$clientid = "e6d71b10-d674-4879-89b3-065069e10c3a"
-$tenantid = "916e52d1-6049-45d7-ba59-46876131ab95"
+$clientid = "Add your clientid here "
+$tenantid = "Add your TenantId here"
 
-#GEt Access Token using MSAL.ps using interactive switch for delegated access 
+#Get Access Token using MSAL.ps using interactive switch for delegated access 
 
-#$MSALtoken = Get-MsalToken -Interactive -ClientId $clientID -TenantId $tenantID
+$MSALtoken = Get-MsalToken -Interactive -ClientId $clientID -TenantId $tenantID
 
 
  #Let's now Import Users info from a CSV File 
- #NOTE: This is where you can get creative and use other API's or integrations to your own source of data.
 
- $users = import-csv -Path "C:\Users\jorlop\OneDrive - Microsoft\Scripts\AZure\jorge.csv"  
+
+ $users = import-csv -Path "path of CSV file"  
  $headers  = @{Authorization = "Bearer $($MSALtoken.accesstoken)" }
  
  #Now that we have a token - Let's POST Authmethods to the users imported from the CSV file  
@@ -30,10 +47,7 @@ $tenantid = "916e52d1-6049-45d7-ba59-46876131ab95"
             $AuthPhonebody = $AuthPhoneArray | ConvertTo-JSON  
     
   $apiUrl = "https://graph.microsoft.com/beta/users/$($userprinname)/authentication/phonemethods/" 
-    #  $apiUrl = "https://graph.microsoft.com/beta/users/$objid/authentication/phoneMethods/3179e48a-750b-4051-897c-87b9720928f7"
-
-    #$Authmethods = Invoke-RestMethod -Uri "https://graph.microsoft.com/beta/users/$($userprinname)/authentication/phonemethods/" -Method Post -Headers $headers -Body $AuthPhonebody
-              
+             
             #build body for the POST command to update auth methods
             $updatephoneparams = @{
             Uri = $apiUrl
@@ -43,7 +57,8 @@ $tenantid = "916e52d1-6049-45d7-ba59-46876131ab95"
             Contenttype = "application/json" 
        
             }  
+           #POST call to Graph 
            $Authmethods = Invoke-RestMethod @updatephoneparams 
-#>
+
     
       }
